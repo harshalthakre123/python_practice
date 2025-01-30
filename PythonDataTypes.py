776b40ac4f01eb86aaf4fd28bda5c77dec26d1bf
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-01-30 18:28:13 +0530

    updatedd

---
+++ PythonDataTypes.py
@@ -0,0 +1,54 @@
+#Python data types====================================================================
+
+####1. Numeric Datatype
+
+# I Integer(datatype--"int")
+a=10
+b=20
+c=a+b
+# print(type(a))
+# print(type(b))
+# print(c, type(c))
+#// -int
+#%---int 
+
+
+
+# II float ()
+d=a/b
+# print(d, type(d))
+
+# II Complex
+x= 4+ 10j
+y= 5+ 9j
+z=x+y
+# print(z, type(z))
+
+
+####2. Collection. Datatype
+
+### Ordered Collection
+# 2. String
+# i-collection of character
+# ii- ordered collection 
+# iii- indexing supported
+# iv- slicing supported
+# v- immutable in nature
+# vi- representation in ('' single line string, " " single line string, ''' '''(Multiline String),)
+
+
+
+# 3. List
+# 4. Tuple
+
+### Unordered collection (Unique element hold)
+# 5. Set
+# 6. Frozenset
+
+###Mapped Data
+# 7. Dictionary
+
+####8.  Boolean
+
+
+
---
+++ TypeCasting.py
@@ -0,0 +1,26 @@
+#Type Casting===========
+# 1. int()
+# 2. str()
+# 3. list()
+# 4. dict()
+# 5. tuple()
+# 6. set()
+# 7. frozenset()
+# 
+# l1=[10, 20, 30, 40] 
+# print (tuple(l1)) #Output-(10, 20, 30, 40)
+# print (type(l1))
+
+# print(set(l1)) #Output-{40, 10, 20, 30}
+# print(frozenset(l1))
+
+#,   #
+
+
+# name= input("Enter your name;")
+# print(name)
+# print(type(name))
+
+age= int(input("Enter your age;"))
+print(age)
+print(type(age))
