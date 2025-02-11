ebb13b1c4a4b81aea6b56080fd58a87aab4cb6fe
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-02-11 23:01:08 +0530

    update

---
+++ Loops/nested for loop.py
@@ -0,0 +1,11 @@
+# outer loop 
+attributes = ['Electric', 'Fast']
+cars = ['Tesla', 'Porsche', 'Mercedes']
+
+for attribute in attributes:
+    for car in cars:
+        print(attribute, car)
+    
+    # this statement is outside the inner loop
+    print("-----")
+  
